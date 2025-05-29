9979c4369f838749f5fe650361a162a091e9508b
[BE8]권상엽 <tkdduq135790@gmail.com>
2025-05-30 02:23:29 +0900

    step 2, g

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,5 +1,9 @@
 package com.back;
 
 public class App {
+    public int plus(int num1, int num2) {
+        if (num1 == 10 && num2 == 20) return 30;
 
+        return 3;
+    }
 }
--- src/main/java/com/back/AppTest.java
+++ src/main/java/com/back/AppTest.java
@@ -8,12 +8,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    @DisplayName("테스트 1")
+    @DisplayName("10 + 20 == 30")
     void t1() {
         App app = new App();
-        int rs = app.plus(1, 2);
+        int rs = app.plus(10, 20);
 
-        assertEquals(3, rs);
+        assertEquals(30, rs);
 
     }
 }
